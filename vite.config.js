e322ea156a40f0096088bd944329fd9376d1f98f
Nam Le <lenam28082000@gmail.com>
2024-07-17 11:30:05 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/FinalProject-CIJS96/",
+  base: "/FinalProject-CIJS96/",
   plugins: [react()],
 })
